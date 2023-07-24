c5bd1caa96489a6256d9b5957852b859809caebb
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-07-24 17:22:27 +0530

    setup all of the views with pug

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use('/admin',adminData.route);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page404.html'));
+    res.status(404).render('page404', {pageTitle: 'Page Not found'});
 });
+
 app.listen(3000);
 
--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const products = [];
 
 // /admin/add-product => GET request
 router.get('/add-product',(req, res, next) => {
-    console.log('Load another middleware!');
+    res.render('add-product', {pageTitle:'Add Product', path: 'admin/add-product'});
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 // /admin/add-product => POST
