17e8bc73408037cfa7b2b3ef2af08168bada5dc8
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-07-27 11:31:12 +0530

    Save the added products in a file in JSON format

--- models/product.js
+++ models/product.js
@@ -1,3 +1,7 @@
+const { json } = require('body-parser');
+const fs = require('fs');
+const path = require ('path');
+
 const products =[];
 
 // create a Product class
@@ -6,11 +10,35 @@ module.exports = class Product{
         this.title = title;
 
     }
+    // save products into a file
     save(){
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename), 
+        'data', 
+        'products.json'
+        );
+        fs.readFile(p,  (err, fileContent) =>{
+            let products = [];
+            if (!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err=>{
+                console.log(err);
+             });
+        });
     }
+    // fetch all product to load in the page
+    static fetchAll(callback){
+        const p = path.join(path.dirname(process.mainModule.filename), 
+        'data', 
+        'products.json'
+        );
+        fs.readFile(p, (err, fileContent) =>{
+            if (err){
+                callback([]);
 
-    static fetchAll(){
-        return products;
+            }
+            callback(JSON.parse(fileContent));
+        });
     }
-}
+};
