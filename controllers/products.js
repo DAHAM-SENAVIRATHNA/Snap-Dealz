68395f063453ba278e1e69db1cee2e991286c0f3
DAHAM-SENAVIRATHNA <nayanads2000@gmail.com>
2023-07-28 22:36:34 +0530

    Add new navigations to navbar

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 const Products = require('../models/product');
 
 exports.getAddProducts =(req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
       formsCSS: true,
@@ -21,7 +21,7 @@ exports.postAddProducts = (req, res, next) => {
 
 exports.showProducts = (req, res, next) => {
     Product.fetchAll(products => {
-      res.render('shop', {
+      res.render('shop/product-list', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
